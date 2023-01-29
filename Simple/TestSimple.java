82701cf2a06bda150365d3ad71a270c031038f84
matthew1060 <62417078+matthew1060@users.noreply.github.com>
2023-01-29 18:46:57 +1100

    Add files via upload

---
+++ Simple/TestSimple.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of TestSimple here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.File;
+
+public class TestSimple{
+     public void print() {  	
+          Simple item = new Simple(3, "blue");     	
+          System.out.println(item);  
+          //System.out.println(item.mystery(5, "ho"));
+     }      
+}
